03b7e67120de4ca22626cdc952f65620cc6c7dc6
Pro3757 <pratyush3757@gmail.com>
2023-11-14 14:51:14 +0530

    topology 1; readme update

--- Assignment 6/readme.md
+++ Assignment 6/readme.md
@@ -1,11 +1,31 @@
 # Packages required
-mininet, openvswitch, python, wireshark
+mininet VM, SSH Client with X11
 # Installation guide
-http://mininet.org/download/#option-1-mininet-vm-installation-easy-recommended 
-(If you're running mininet ubuntu server through VM, it may require you to use x11 (startx) as ubuntu server lacks GUI to use wiresharks etc.)
+[Guide link](http://mininet.org/download/#option-1-mininet-vm-installation-easy-recommended) 
+
+If you're running mininet ubuntu server through VM,  
+you would need a ssh client and X11 on your client machine to use graphical programs like wireshark.
+# Setup VM and SSH into it
+Virtual Machine Settings -> Network -> Attached to: Host-only Adapter
+
+In VM:
+```bash
+$ ip addr
+```
+Write down the ip address of the ethX interface. It should look like `192.168.X.Y`
+
+On client:
+```bash
+$ ssh -Y mininet@192.168.X.Y
+```
+
 # Run
-bash> sudo python <filename.py> 
-(It will execute: adding controllers, adding switches, adding hosts etc.)
-(Then)
+```bash
+$ sudo python <filename.py>
+```
+It will execute: adding controllers, adding switches, adding hosts etc.
+
+```bash
 mininet>
-(Now you may try several options listed by typing help)
+```
+Now you may try several options listed by typing help
--- Assignment 6/topology_part1.py
+++ Assignment 6/topology_part1.py
@@ -1,78 +1,112 @@
+#!/usr/bin/env python
+
+"""
+topo1.py: Network for question 1 with Linux IP routers
+
+Converts a Node into a router using IP forwarding
+already built into Linux.
+
+The topology creates 3 routers and 4 IP subnets:
+
+    - 192.168.1.0/24 (ra-h1-h2, IP: 192.168.1.{1, 100, 101})
+    - 192.168.1.0/24 (rb-h3-h4, IP: 192.168.1.{1, 100, 101})
+    - 192.168.1.0/24 (rc-h5-h6, IP: 192.168.1.{1, 100, 101})
+    - 10.0.0.0/8 (ra-rb-rc, IP: 10.0.0.{1, 2, 3})
+
+The example relies on default routing entries that are
+automatically created for each router interface, as well
+as 'defaultRoute' parameters for the host interfaces.
+
+Additional routes may be added to the router or hosts by
+executing 'ip route' or 'route' commands on the router or hosts.
+"""
+
+
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Controller, OVSSwitch
-from mininet.cli import CLI
+from mininet.node import Node
 from mininet.log import setLogLevel, info
+from mininet.cli import CLI
 
-class CustomTopo(Topo):
-    "Custom topology with 3 routers (switch+host) and 6 hosts."
 
-    def build(self):
-        # Add hosts and switches
-        hosts = [self.addHost('h{}'.format(i)) for i in range(1, 7)]
-        switches = [self.addSwitch('s{}'.format(i)) for i in range(1, 4)]
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
 
-        # Add links between hosts and switches
-        for i in range(3):
-            self.addLink(hosts[i*2], switches[i])
-            self.addLink(hosts[i*2+1], switches[i])
-	    
+    # pylint: disable=arguments-differ
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
 
-        # Add links between switches
-        self.addLink(switches[0], switches[1])
-        self.addLink(switches[1], switches[2])
-        #self.addLink(switches[0], switches[2])
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
 
+
+class NetworkTopo( Topo ):
+    "Topology for question 1"
+
+    # pylint: disable=arguments-differ
+    def build( self, **_opts ):
+
+        eth0 = '192.168.1.1/24'  # IP address for rx-eth0
+        
+        host_IP_1 = '192.168.1.100/24'
+        host_IP_2 = '192.168.1.101/24'
+        host_route = 'via 192.168.1.1'
+
+        ra = self.addNode( 'ra', cls=LinuxRouter, ip=eth0 )
+        rb = self.addNode( 'rb', cls=LinuxRouter, ip=eth0 )
+        rc = self.addNode( 'rc', cls=LinuxRouter, ip=eth0 )
+
+        s1, s2, s3, s4 = [ self.addSwitch( s ) for s in ( 's1', 's2', 's3', 's4' ) ]
+        
+        self.addLink( s1, ra, intfName2='ra-eth0',
+                      params2={ 'ip' : eth0 } )
+        self.addLink( s2, rb, intfName2='rb-eth0',
+                      params2={ 'ip' : eth0 } )
+        self.addLink( s3, rc, intfName2='rc-eth0',
+                      params2={ 'ip' : eth0 } )
+        
+        self.addLink( s4, ra, intfName2='ra-eth1',
+                      params2={ 'ip' : '10.0.0.1/8' } )
+        self.addLink( s4, rb, intfName2='rb-eth1',
+                      params2={ 'ip' : '10.0.0.2/8' } )
+        self.addLink( s4, rc, intfName2='rc-eth1',
+                      params2={ 'ip' : '10.0.0.3/8' } )
+        
+        h1 = self.addHost( 'h1', ip= host_IP_1,
+                           defaultRoute=host_route )
+        h2 = self.addHost( 'h2', ip=host_IP_2,
+                           defaultRoute=host_route )
+        h3 = self.addHost( 'h3', ip=host_IP_1,
+                           defaultRoute=host_route )
+        h4 = self.addHost( 'h4', ip=host_IP_2,
+                           defaultRoute=host_route )        
+        h5 = self.addHost( 'h5', ip=host_IP_1,
+                           defaultRoute=host_route )
+        h6 = self.addHost( 'h6', ip=host_IP_2,
+                           defaultRoute=host_route )
+        
+        self.addLink(h1,s1)
+        self.addLink(h2,s1)
+        self.addLink(h3,s2)
+        self.addLink(h4,s2)
+        self.addLink(h5,s3)
+        self.addLink(h6,s3)
+        
 def run():
-    "Create and run the network"
-    topo = CustomTopo()
-    net = Mininet(topo=topo, controller=Controller, switch=OVSSwitch)
+    "Test the topology"
+    topo = NetworkTopo()
+    net = Mininet( topo=topo,
+                   waitConnected=True )  # controller is used by switches
     net.start()
-    h1, h2, h6, r1, r2, r3 = net.get('h1', 'h2','h6', 's1', 's2', 's3')
-
-    r1.cmd('tcpdump -i s1-eth1 -w r1.pcap &')  # replace 's1-eth1' with the name of the interface you want to capture on
-    # Assign IP addresses
-    for i in range(1, 7):
-        net.get('h{}'.format(i)).setIP('192.168.{}.{}'.format((i-1)//2+1, 100+(i-1)%2), intf='h{}-eth0'.format(i))
-
-    print("Ping h1 to h2: ")
-
-    print(h1.cmd('ping -c 4 %s' % h2.IP()))
-	
-
-
-
-    # Measure the latency with ping
-    print('Ping latency:')
-    print(h1.cmd('ping -c 4 %s' % h6.IP()))
-    
-
-    # Measure the latency with iperf
-    print('Iperf latency:')
-    h6.cmd('iperf -s &')  # start iperf server on h6
-    iperf_print=h1.cmd('iperf -c %s -t 10' % h6.IP())  # start iperf client on h1
-    print(iperf_print)
-   
-    print("Changing routes...")
-
-    h1.cmd('ip route add default via %s' % r1.IP())
-    r1.cmd('ip route add default via %s' % r2.IP())
-    r2.cmd('ip route add default via %s' % r3.IP())
-    r3.cmd('ip route add default via %s' % h6.IP())
-    print('Ping latency:')
-    print(h1.cmd('ping -c 4 %s' % h6.IP()))
-    
-
-    # Measure the latency with iperf
-    print('Iperf latency:')
-
-    iperf_print=h1.cmd('iperf -c %s -t 10' % h6.IP())  # start iperf client on h1
-    print(iperf_print)
-    CLI(net)
-    r1.cmd('pkill -SIGINT tcpdump')
+    info( '*** Routing Table on Router:\n' )
+    info( net[ 'ra' ].cmd( 'route' ) )
+    CLI( net )
     net.stop()
 
 
 if __name__ == '__main__':
-    setLogLevel('info')
+    setLogLevel( 'info' )
     run()
